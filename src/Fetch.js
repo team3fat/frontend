648c4988c25119a03809b548be90a7c78d69d381
Diego <d.gimenez0101@gmail.com>
2019-05-16 13:23:12 -0300

    Testing fetch

---
+++ src/Fetch.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+
+const API = 'https://hn.algolia.com/api/v1/search?query=';
+const DEFAULT_QUERY = 'redux';
+
+class Fetch extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      hits: [],
+    };
+  }
+
+  componentDidMount() {
+    fetch(API + DEFAULT_QUERY)
+      .then(response => response.json())
+      .then(data => this.setState({ hits: data.hits }));
+  }
+}
+
+export default Fetch;
--- src/index.js
+++ src/index.js
@@ -4,10 +4,12 @@ import './index.css';
 import Register from './Register';
 import * as serviceWorker from './serviceWorker';
 import Login from './Login';
+import Fetch from './Fetch,js';
 
 const webpage = <Login />;
+const pageFetch = <Fetch />;
 
-ReactDOM.render(webpage, document.getElementById('root'));
+ReactDOM.render(pageFetch, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
