a23b7777ea580c31a2886c574c308f992d616304
Diego <d.gimenez0101@gmail.com>
2019-05-16 13:25:26 -0300

    Corrected import

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import './index.css';
 import Register from './Register';
 import * as serviceWorker from './serviceWorker';
 import Login from './Login';
-import Fetch from './Fetch,js';
+import Fetch from './Fetch.js';
 
 const webpage = <Login />;
 const pageFetch = <Fetch />;
