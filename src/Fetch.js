55c299d4e8a93f7f574895aea6cacba246ea884d
Diego <d.gimenez0101@gmail.com>
2019-05-17 21:58:48 -0300

    Fetched

--- src/Fetch.js
+++ src/Fetch.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-
 const API = 'https://hn.algolia.com/api/v1/search?query=';
 const DEFAULT_QUERY = 'redux';
 
@@ -9,26 +8,28 @@ class Fetch extends Component {
 
         this.state = {
             hits: [],
-            isLoading: false,
-            error: null,
         };
     }
   
     componentDidMount() {
-
-        this.setState({ isLoading: true });
-
         fetch(API + DEFAULT_QUERY)
         .then(response => response.json())
-        .then(data => this.setState({hits: data.hits, isLoading: false}))
-        .catch(error => this.setState({error, isLoading: false}));
-    };
-
-    render(){
-        return (
-            this.state.hits.val()
-        )
+        .then(data => this.setState({ hits: data.hits }));
     }
+
+    render() {
+        const { hits } = this.state;
+    
+        return (
+          <ul>
+            {hits.map(hit =>
+              <li key={hit.objectID}>
+                <a href={hit.url}>{hit.title}</a>
+              </li>
+            )}
+          </ul>
+        );
+      }
 }
 
 export default Fetch;
