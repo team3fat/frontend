723d91926ca9c52196dcb96aedbb73032026f9f4
diegogimenez04 <d.gimenez0101@gmail.com>
2019-07-31 09:57:12 -0300

    Added css

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
 import React from 'react';
-//import { BrowseRouter } from 'react-router-dom';
+import {GoogleLogin, GoogleLogout, FontAwesome} from 'react-google-login';
 
 function App(){
+
+    const responseGoogle = (response) => {
+        console.log(response)
+    }
+
     return (
             <div>
-                Ahora si empezamos
+                <GoogleLogin
+                    clientId={'658977310896-knrl3gka66fldh83dao2rhgbblmd4un9.apps.googleusercontent.com'}
+                    onSuccess={responseGoogle}
+                    onFailure={responseGoogle}
+                >
+                    <FontAwesome
+                    name='google'
+                    />
+                    <span> Login with Google</span>
+                </GoogleLogin>
             </div>
     )
 };
