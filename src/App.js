ccaf00062ae589b9c2a9132d59bf8be0637a76b9
diegogimenez04 <d.gimenez0101@gmail.com>
2019-07-31 12:03:09 -0300

    Imported google

--- src/App.js
+++ src/App.js
@@ -9,16 +9,7 @@ function App(){
 
     return (
             <div>
-                <GoogleLogin
-                    clientId={'658977310896-knrl3gka66fldh83dao2rhgbblmd4un9.apps.googleusercontent.com'}
-                    onSuccess={responseGoogle}
-                    onFailure={responseGoogle}
-                >
-                    <FontAwesome
-                    name='google'
-                    />
-                    <span> Login with Google</span>
-                </GoogleLogin>
+                
             </div>
     )
 };
