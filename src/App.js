d1ac8d55e84a6929db1fc5079822c8f7a6a32db8
diegogimenez04 <d.gimenez0101@gmail.com>
2019-08-01 09:35:53 -0300

    Created component NavBar

--- src/App.js
+++ src/App.js
@@ -1,14 +1,9 @@
 import React from 'react';
 
 function App(){
-
-    const responseGoogle = (response) => {
-        console.log(response)
-    }
-
     return (
             <div>
-                <t>Hola</t>
+                
             </div>
     )
 };
--- src/_components/nav-bar/nav-bar.js
+++ src/_components/nav-bar/nav-bar.js
@@ -1,12 +1,32 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { NavLink } from 'react-router-dom';
 
 class NavBar extends Component {
     render() {
         return (
             <nav>
-                <NavLink></NavLink>
+                <NavLink >Inicio</NavLink>
+                <NavLink >Informacion</NavLink>
+                <NavLink >Reservas</NavLink>
             </nav>
         )
     }
+
+    getNavLinkProps(specificProp){
+        var props = {
+            inicio: {
+                to: '/'
+            },
+            Informacion: {
+                to: '/info'
+            },
+            reservas: {
+                to: '/reservas'
+            }
+        }
+
+        return props[specificProp]
+    }
 }
+
+export default NavBar;
