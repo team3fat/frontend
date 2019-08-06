f51c18769af868cf74ad091db5f201813f9b39f0
diegogimenez04 <d.gimenez0101@gmail.com>
2019-08-06 11:17:15 -0300

    Testing fetch

--- src/_scenes/Reservas/Reservas.js
+++ src/_scenes/Reservas/Reservas.js
@@ -2,6 +2,11 @@ import React, { Component } from 'react';
 import Calendar from 'react-calendar';
 
 export default class Reservas extends Component {
+    componentDidMount(){
+        fetch('www.google.com')
+         .then(response => response.json())
+         .then(json => console.log(json))
+    }
     render() {
         return (
             <div>
