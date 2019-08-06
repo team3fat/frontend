fb5cd202641ea17e6ae752c2dd6b1a121e4af135
diegogimenez04 <d.gimenez0101@gmail.com>
2019-08-06 11:35:16 -0300

    Added calendar, trying to use fetch

--- src/_scenes/Reservas/Reservas.js
+++ src/_scenes/Reservas/Reservas.js
@@ -2,14 +2,23 @@ import React, { Component } from 'react';
 import Calendar from 'react-calendar';
 
 export default class Reservas extends Component {
+    
+    constructor(props){
+        super(props);
+        this.state={
+            gender: null
+        };
+    }
+    
     componentDidMount(){
-        fetch('www.google.com')
+        fetch("https://randomuser.me/api/")
          .then(response => response.json())
-         .then(json => console.log(json))
+         .then(json => console.log(json.results[0].name)) //console.log(json.results[0].gender)
     }
     render() {
         return (
             <div>
+                <a>{this.props.gender}</a>
                 <Calendar
                     minDate={new Date()}
                 />
