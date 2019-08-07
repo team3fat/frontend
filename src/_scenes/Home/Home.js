fdf21ff76394fbb5af7af29630d7c2762b66ece6
LeoMoreno2001 <leoladocta10@gmail.com>
2019-08-07 09:33:26 -0300

    NavBar Fake XD

--- src/_scenes/Home/Home.js
+++ src/_scenes/Home/Home.js
@@ -1,12 +1,38 @@
-import React, { Component } from 'react'
-export default class Home extends Component {
-    render() {
-        return (
-            <div>
-                <header>
-                    <t>this is home</t>
-                </header>
-            </div>
-        )
-    }
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import Button from '@material-ui/core/Button';
+import IconButton from '@material-ui/core/IconButton';
+import MenuIcon from '@material-ui/icons/Menu';
+
+const useStyles = makeStyles(theme => ({
+  root: {
+    flexGrow: 1,
+  },
+  menuButton: {
+    marginRight: theme.spacing(2),
+  },
+  title: {
+    flexGrow: 1,
+  },
+}));
+
+export default function ButtonAppBar() {
+  const classes = useStyles();
+
+  return (
+    <div className={classes.root}>
+      <AppBar position="static">
+        <Toolbar>
+          <Typography variant="h6" className={classes.title}>
+            Campamento Diquecito
+          </Typography>
+          <Button color="inherit">Registrate</Button>
+          <Button color="inherit">Iniciar Sesion</Button>
+        </Toolbar>
+      </AppBar>
+    </div>
+  );
 }
