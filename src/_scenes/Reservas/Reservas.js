8ec8359ced66b3a068050f9cc5ac1a4625ccea6a
diegogimenez04 <d.gimenez0101@gmail.com>
2019-08-07 09:59:43 -0300

    Added simple fetch

--- src/_scenes/Reservas/Reservas.js
+++ src/_scenes/Reservas/Reservas.js
@@ -6,19 +6,19 @@ export default class Reservas extends Component {
     constructor(props){
         super(props);
         this.state={
-            gender: null
+            name: null
         };
     }
     
     componentDidMount(){
         fetch("https://randomuser.me/api/")
          .then(response => response.json())
-         .then(json => this.setState({gender: json.results[0].name.first})) //console.log(json.results[0].gender)
+         .then(json => this.setState({name: json.results[0].name.first})) //console.log(json.results[0].name)
     }
     render() {
         return (
             <div>
-                <a>Bienvenido/a {this.state.gender}</a>
+                <a>Bienvenido/a {this.state.name}</a>
                 <Calendar
                     minDate={new Date()}
                 />
