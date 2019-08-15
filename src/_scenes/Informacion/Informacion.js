b83cd7181477574906e5850289f71fd8b96c319e
BrunoCavazza <bruno.boomvazza@gmail.com>
2019-08-15 13:00:24 -0300

    mapa

--- src/_scenes/Informacion/Informacion.js
+++ src/_scenes/Informacion/Informacion.js
@@ -1,11 +1,64 @@
-import React, { Component } from 'react'
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import Grid from '@material-ui/core/Grid';
+import Paper from '@material-ui/core/Paper';
+import Typography from '@material-ui/core/Typography';
+import ButtonBase from '@material-ui/core/ButtonBase';
 
-export default class Informacion extends Component {
-    render() {
-        return (
-            <div>
-                <t>Hola</t>
-            </div>
-        )
-    }
+const useStyles = makeStyles(theme => ({
+  root: {
+    flexGrow: 1,
+  },
+  paper: {
+    padding: theme.spacing(2),
+    margin: 'auto',
+    maxWidth: 500,
+  },
+  image: {
+    width: 128,
+    height: 128,
+  },
+  img: {
+    margin: 'auto',
+    display: 'block',
+    maxWidth: '100%',
+    maxHeight: '100%',
+  },
+}));
+
+export default function ComplexGrid() {
+  const classes = useStyles();
+
+  return (
+    <div className={classes.root}>
+      <Paper className={classes.paper}>
+        <Grid container spacing={2}>
+          <Grid item>
+            <ButtonBase className={classes.image}>
+              <img className={classes.img} alt="complejo" src="/_assets/Img/Map.jpg"/>
+            </ButtonBase>
+          </Grid>
+          <Grid item xs={12} sm container>
+            <Grid item xs container direction="column" spacing={2}>
+              <Grid item xs>
+                <Typography gutterBottom variant="subtitle1">
+                  Complejo Diquecito
+                </Typography>
+                <Typography variant="body2" gutterBottom>
+                <a href="https://www.google.com/maps/place/Nuevo+Complejo+Diquecito+(campamento+evangelico)/@-31.3589483,-64.3826017,149a,35y,181.59h,44.73t/data=!3m1!1e3!4m5!3m4!1s0x9432a2891aac0737:0x55e26374f614987c!8m2!3d-31.3606491!4d-64.3822378?hl=es">Google Maps</a>
+                </Typography>
+                <Typography variant="body2" color="textSecondary">
+                <a href="https://www.google.com/maps/dir/-31.3607357,-64.3824555/-31.3607839,-64.383071/@-31.3591662,-64.382636,150a,35y,181.59h,44.73t/data=!3m1!1e3!4m2!4m1!3e2?hl=es">Como Llegar</a>
+                </Typography>
+              </Grid>
+              <Grid item>
+              </Grid>
+            </Grid>
+            <Grid item>
+            </Grid>
+          </Grid>
+        </Grid>
+      </Paper>
+    </div>
+  );
 }
