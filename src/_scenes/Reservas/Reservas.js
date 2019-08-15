15e24e6877d2dca137fd6a79127f732de0082ac0
diegogimenez04 <d.gimenez0101@gmail.com>
2019-08-15 11:10:08 -0300

    Skipper genio

--- src/_scenes/Reservas/Reservas.js
+++ src/_scenes/Reservas/Reservas.js
@@ -8,7 +8,7 @@ export default class Reservas extends Component {
 
     constructor(props){
         super(props);
-        this.state={
+        this.state = {
             name: null,
             diaInicio: null,
             diaFin: null,
@@ -17,36 +17,58 @@ export default class Reservas extends Component {
         };    
     }
 
-    
-
-    selectDia(event){
-        console.log(event.getDate());
-        this.setState({});
-        console.log("Pepe")
-    }
-    
-    componentWillMount(){
-        fetch("https://randomuser.me/api/")
-         .then(response => response.json())
-         .then(json => this.setState({name: json.results[0].name.first})) //console.log(json.results[0].name)        
-        
-    }
-
     render() {
         return (
             <div>
                 <a>Bienvenido/a {this.state.name}</a>
                 <p>{this.state.dia}</p>
-                <Calendar
-                    minDate={new Date()}
-                    onClickDay={this.selectDia.bind(this)}
-                />
-                <FormControl>
-                    <Input id="diaInicio" placeholder="Desde"></Input>
-                    <Input id="diaFin" placeholder="Hasta"></Input>
-                </FormControl>
+                {this.renderCalendar()}
+                {this.renderFormControl()}
             </div>
-        )
+        );
+    }
+
+    renderCalendar() {
+        return (
+            <Calendar {...this.getCalendarProps()} />
+        );
+    }
+
+    renderFormControl() {
+        return (
+            <FormControl>
+                <Input id="diaInicio" placeholder="Desde"></Input>
+                <Input id="diaFin" placeholder="Hasta"></Input>
+            </FormControl>
+        );
+    }
+
+    getCalendarProps() {
+        return {        
+            minDate: new Date(),
+            onClickDay: this.selectDia.bind(this)
+        }
+    }
+
+    returnLista(dia) {
+        let diasTotales = this.state.diasTotales
+        diasTotales.push(dia)
+        return diasTotales
+    }
+
+    selectDia(event){
+        console.log(event.getDate());
+        this.setState({
+            diasTotales: this.returnLista(event.getDate())
+        });
+        console.log(this.state.diasTotales)
+    }
+
+    componentWillMount(){
+        fetch("https://randomuser.me/api/")
+         .then(response => response.json())
+         .then(json => this.setState({name: json.results[0].name.first})) //console.log(json.results[0].name)        
+        
     }
 
     pepeCallback() {
