f13f0a8fbb7a255ee165341776ff3c809baf117b
diegogimenez04 <d.gimenez0101@gmail.com>
2019-08-28 11:26:17 -0300

    POST to test

--- src/_scenes/Reservas/Reservas.js
+++ src/_scenes/Reservas/Reservas.js
@@ -1,25 +1,32 @@
 import React, { Component } from 'react';
 import FormControl from '@material-ui/core/FormControl';
-import Input from '@material-ui/core/Input';
 import Button from '@material-ui/core/Button';
 import DateRangePicker from '@wojtekmaj/react-daterange-picker';
+var moment = require('moment');
 
 export default class Reservas extends Component {
 
     constructor(props) {
         super(props);
         this.state = {
-            name: null,
-            descripcion: null,
+            diasComienzo: [],
+            diasFinal: [],
             estado: null,
-            diasTotales: [new Date(), new Date()],
+            estados: null,
+            diasAReservar: [new Date(), new Date()],
         };
+    }
+
+    componentWillMount() {
+        fetch("http://127.0.0.1:8000/diquecito/calendario/")
+            .then(response => response.json())
+            .then(json => { this.getDiasReservados(json) })
     }
 
     render() {
         return (
             <div>
-                <a>Bienvenido/a {this.state.name}</a>
+                <h1>Hace tu reserva aca</h1>
                 <p>{this.state.dia}</p>
                 {this.renderCalendar()}
                 {this.renderFormControl()}
@@ -33,7 +40,7 @@ export default class Reservas extends Component {
                 <DateRangePicker
                     minDate={new Date()}
                     onChange={this.onChange}
-                    value={this.state.diasTotales}
+                    value={this.state.diasAReservar}
                     format="dd-MM-y"
                 />
             </div>
@@ -42,24 +49,63 @@ export default class Reservas extends Component {
 
     renderFormControl() {
         return (
-            <FormControl>
-                <h4>Desde</h4>
-                <Input id="diaInicio" placeholder="Desde"></Input>
-                <h4>Hasta</h4>
-                <Input id="diaFin" placeholder="Hasta"></Input>
-                <h4>Ingrese una breve descripcion por favor</h4>
-                <Input id="descripcionReserva" placeholder="Ingrese una descripcion"></Input>
-                <Button onClick="submit" color="primary">Reservar!</Button>
+            <FormControl method="POST">
+                <Button onClick={() => { this.onSubmit() }} color="primary">Reservar!</Button>
             </FormControl>
         );
     }
 
-    onChange = date => this.setState({ date })
+    onChange = date => this.setState({ diasAReservar: date })
 
-    componentWillMount() {
-        fetch("https://randomuser.me/api/")
-            .then(response => response.json())
-            .then(json => this.setState({ name: json.results[0].name.first })) //console.log(json.results[0].name)        
-
+    onSubmit() {
+        var comienzo = this.state.diasAReservar[0]
+        var final = this.state.diasAReservar[1]
+        var estado = "PEDIDO"
+        var objeto = {
+            comienzo: this.transformarFecha(comienzo),
+            final: this.transformarFecha(final),
+            estado: estado
+        }
+        var config = {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: objeto,
+            credentials: 'omit',
+            mode: 'cors',
+            cache: 'no-cache'
+        }
+        console.log(objeto)
+        fetch("http://127.0.0.1:8000/diquecito/reservation/", config)
+            // .then(res => res.json())
+            .then(resp => {
+                if (!resp.ok) {
+                    console.log("Error!", resp)
+                } else {
+                    console.log(resp)
+                }
+            })
+            .catch(err => console.log(("Error! This is the error:", err)))
     }
+
+    getDiasReservados(json) {
+        var diasComienzo = [];
+        var diasFinal = [];
+        var estados = [];
+        json.forEach(x => {
+            diasComienzo.push(x.comienzo)
+            diasFinal.push(x.final)
+            estados.push(x.estado)
+            console.log(x)
+        });
+        this.setState({ diasComienzo: diasComienzo, diasFinal: diasFinal, estados: estados })
+        console.log(this.state.diasComienzo)
+        console.log(this.state.diasFinal)
+        console.log(this.state.estados)
+        return (diasComienzo, diasFinal, estados)
+    }
+
+    transformarFecha(fecha) {
+        return moment(fecha).format('MM-DD-YYYY')
+    }
+
 }
