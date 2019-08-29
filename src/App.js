36df13f92afb57abb818ae986112f90f7460fa6a
LeoMoreno2001 <leoladocta10@gmail.com>
2019-08-29 12:41:32 -0300

    Falta Tarjetas

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import AppBar from '@material-ui/core/AppBar';
 import AppRoutes from './routes.js';
 import Button from '@material-ui/core/Button';
 import { BrowserRouter } from 'react-router-dom';
+import CssBaseline from '@material-ui/core/CssBaseline';
 import Footer from './_components/PieDePagina/Footer.js';
 import Grid from '@material-ui/core/Grid';
 import { makeStyles } from '@material-ui/core/styles';
@@ -33,6 +34,7 @@ function App() {
             direction="column"
             justify="center"
             alignItems="center">
+            <CssBaseline>
             <AppBar position="static">
                 <Toolbar>
                     <Typography variant="h6" className={classes.title}>
@@ -42,6 +44,7 @@ function App() {
                     <Button color="inherit" href="/ingreso">Iniciar Sesion</Button>
                 </Toolbar>
             </AppBar>
+            </CssBaseline>
             <BrowserRouter>
                 <div className="main">
                     <section className="main--section">
