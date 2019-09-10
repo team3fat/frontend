2ae0d04f24eeb771103fad5966dffc6018f257c4
WEIK ME AP <30444439+diegogimenez04@users.noreply.github.com>
2019-09-10 11:25:17 -0300

    Revert "Feature/50 modificar tamanio y color del calendario"

--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import * as serviceWorker from './serviceWorker.js';
-import App from './App.js';
+import App from './App';
+import './index.css';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-
-serviceWorker.unregister();
+ReactDOM.render(
+  <App />,
+  document.getElementById('root')
+);
