58a471340337b324f5723bb7f574b1b4ed4e9d67
BrunoCavazza <bruno.boomvazza@gmail.com>
2019-10-08 10:19:39 -0300

    login

--- src/_scenes/Ingreso/Ingreso.js
+++ src/_scenes/Ingreso/Ingreso.js
@@ -1,9 +1,9 @@
 import React from 'react';
-import clsx from 'clsx';
 import { makeStyles } from '@material-ui/core/styles';
-import MenuItem from '@material-ui/core/MenuItem';
 import TextField from '@material-ui/core/TextField';
 import Paper from '@material-ui/core/Paper';
+import { Grid } from '@material-ui/core';
+import Button from '@material-ui/core/Button';
 
 
 const useStyles = makeStyles(theme => ({
@@ -16,45 +16,41 @@ const useStyles = makeStyles(theme => ({
     marginRight: theme.spacing(1),
     width: 200,
   },
+  button: {
+    margin: theme.spacing(1),
+  },
   dense: {
     marginTop: 19,
   },
   menu: {
     width: 200,
   },
+  title: {
+    padding: theme.spacing(0),
+    textAlign: 'center',
+    minHeight: '35px',
+    minWidth: "280px",
+    marginTop: "35px",
+    background: '#3949ab',
+  },  
   paper: {
     padding: theme.spacing(0),
     textAlign: 'center',
     margin: "50px",
+    minHeight: "200px",
+    minWidth: "400px",
   },
+  root: {
+    height: "calc(100vh -64px)"
+  }
 }));
-
-const currencies = [
-  {
-    value: 'USD',
-    label: '$',
-  },
-  {
-    value: 'EUR',
-    label: '€',
-  },
-  {
-    value: 'BTC',
-    label: '฿',
-  },
-  {
-    value: 'JPY',
-    label: '¥',
-  },
-];
 
 export default function TextFields() {
   const classes = useStyles();
   const [values, setValues] = React.useState({
-    name: 'Cat in the Hat',
+    name: '',
     age: '',
     multiline: 'Controlled',
-    currency: 'EUR',
   });
 
   const handleChange = name => event => {
@@ -63,196 +59,57 @@ export default function TextFields() {
 
   return (
     <div className={classes.root}>
+      <Grid container
+                direction="row"
+                justify="center"
+                alignItems="center">
+        <Paper className={classes.title} square={true}>
+          <h5 className="colortext MuiTypography-root makeStyles-title-3 MuiTypography-h6" >Iniciar sesion</h5>
+         </Paper>
+        </Grid>
         <Paper className={classes.paper} square={true}>
-        <h5 className="colortext2 MuiTypography-root makeStyles-title-3 MuiTypography-h6">Iniciar Sesion</h5>
-            <form className={classes.container} noValidate autoComplete="off">
-            <TextField
-                id="standard-name"
-                label="Name"
-                className={classes.textField}
-                value={values.name}
-                onChange={handleChange('name')}
-                margin="normal"
-            />
-            <TextField
-                id="standard-uncontrolled"
-                label="Uncontrolled"
-                defaultValue="foo"
-                className={classes.textField}
-                margin="normal"
-            />
-            <TextField
-                required
-                id="standard-required"
-                label="Required"
-                defaultValue="Hello World"
-                className={classes.textField}
-                margin="normal"
-            />
-            <TextField
-                error
-                id="standard-error"
-                label="Error"
-                defaultValue="Hello World"
-                className={classes.textField}
-                margin="normal"
-            />
-            <TextField
-                disabled
-                id="standard-disabled"
-                label="Disabled"
-                defaultValue="Hello World"
-                className={classes.textField}
-                margin="normal"
-            />
-            <TextField
-                id="standard-password-input"
-                label="Password"
-                className={classes.textField}
-                type="password"
-                autoComplete="current-password"
-                margin="normal"
-            />
-            <TextField
-                id="standard-read-only-input"
-                label="Read Only"
-                defaultValue="Hello World"
-                className={classes.textField}
-                margin="normal"
-                InputProps={{
-                readOnly: true,
-                }}
-            />
-            <TextField
-                id="standard-dense"
-                label="Dense"
-                className={clsx(classes.textField, classes.dense)}
-                margin="dense"
-            />
-            <TextField
-                id="standard-multiline-flexible"
-                label="Multiline"
-                multiline
-                rowsMax="4"
-                value={values.multiline}
-                onChange={handleChange('multiline')}
-                className={classes.textField}
-                margin="normal"
-            />
-            <TextField
-                id="standard-multiline-static"
-                label="Multiline"
-                multiline
-                rows="4"
-                defaultValue="Default Value"
-                className={classes.textField}
-                margin="normal"
-            />
-            <TextField
-                id="standard-helperText"
-                label="Helper text"
-                defaultValue="Default Value"
-                className={classes.textField}
-                helperText="Some important text"
-                margin="normal"
-            />
-            <TextField
-                id="standard-with-placeholder"
-                label="With placeholder"
-                placeholder="Placeholder"
-                className={classes.textField}
-                margin="normal"
-            />
-            <TextField
-                id="standard-textarea"
-                label="With placeholder multiline"
-                placeholder="Placeholder"
-                multiline
-                className={classes.textField}
-                margin="normal"
-            />
-            <TextField
-                id="standard-number"
-                label="Number"
-                value={values.age}
-                onChange={handleChange('age')}
-                type="number"
-                className={classes.textField}
-                InputLabelProps={{
-                shrink: true,
-                }}
-                margin="normal"
-            />
-            <TextField
-                id="standard-search"
-                label="Search field"
-                type="search"
-                className={classes.textField}
-                margin="normal"
-            />
-            <TextField
-                id="standard-select-currency"
-                select
-                label="Select"
-                className={classes.textField}
-                value={values.currency}
-                onChange={handleChange('currency')}
-                SelectProps={{
-                MenuProps: {
-                    className: classes.menu,
-                },
-                }}
-                helperText="Please select your currency"
-                margin="normal"
-            >
-                {currencies.map(option => (
-                <MenuItem key={option.value} value={option.value}>
-                    {option.label}
-                </MenuItem>
-                ))}
-            </TextField>
-            <TextField
-                id="standard-select-currency-native"
-                select
-                label="Native select"
-                className={classes.textField}
-                value={values.currency}
-                onChange={handleChange('currency')}
-                SelectProps={{
-                native: true,
-                MenuProps: {
-                    className: classes.menu,
-                },
-                }}
-                helperText="Please select your currency"
-                margin="normal"
-            >
-                {currencies.map(option => (
-                <option key={option.value} value={option.value}>
-                    {option.label}
-                </option>
-                ))}
-            </TextField>
-            <TextField
-                id="standard-full-width"
-                label="Label"
-                style={{ margin: 8 }}
-                placeholder="Placeholder"
-                helperText="Full width!"
-                fullWidth
-                margin="normal"
-                InputLabelProps={{
-                shrink: true,
-                }}
-            />
-            <TextField
-                id="standard-bare"
-                className={classes.textField}
-                defaultValue="Bare"
-                margin="normal"
-                inputProps={{ 'aria-label': 'bare' }}
-            />
-            </form>
+              <form className={classes.container} noValidate autoComplete="off">
+                <Grid
+                container
+                direction="column"
+                justify="center"
+                alignItems="center"
+                  >
+                <Grid item xs={5}>
+                  <TextField
+                      id="standard-name"
+                      label="Usuario"
+                      className={classes.textField}
+                      value={values.name}
+                      onChange={handleChange('name')}
+                      margin="normal"
+                  />
+                </Grid>
+                <Grid item xs={5}>
+                   <TextField
+                      id="standard-password-input"
+                      label="Contraseña"
+                      className={classes.textField}
+                      type="password"
+                      autoComplete="current-password"
+                      margin="normal"
+                  />
+                 </Grid>
+                </Grid>
+                <Grid
+                    container
+                    direction="row"
+                    justify="center"
+                    alignItems="center"
+                  >
+                  <Button variant="contained" color="primary" className={classes.button}>
+                    Registrarse
+                  </Button>
+                  <Button variant="contained" color="primary" className={classes.button}>
+                    Inicir Sesión
+                  </Button>
+                </Grid>
+              </form>
         </Paper>
     </div>
   );
