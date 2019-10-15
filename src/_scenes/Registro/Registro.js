a25a2ea320c3b275265edc3f22520373504bc3fa
Leo Moreno <leoladocta10@gmail.com>
2019-10-15 10:51:48 -0300

    formularioRegistro

--- src/_scenes/Registro/Registro.js
+++ src/_scenes/Registro/Registro.js
@@ -3,9 +3,9 @@ import clsx from 'clsx';
 import { makeStyles } from '@material-ui/core/styles';
 import TextField from '@material-ui/core/TextField';
 import Paper from '@material-ui/core/Paper';
-import List from '@material-ui/core/List';
 import ListItem from '@material-ui/core/ListItem';
 import Grid from '@material-ui/core/Grid'
+import Button from '@material-ui/core/Button';
 
 const useStyles = makeStyles(theme => ({
   container: {
@@ -17,13 +17,32 @@ const useStyles = makeStyles(theme => ({
     marginRight: theme.spacing(1),
     width: 200,
   },
+  button: {
+    margin: theme.spacing(1),
+  },
   dense: {
     marginTop: 19,
   },
+  menu: {
+    width: 200,
+  },
+  title: {
+    padding: theme.spacing(0),
+    textAlign: 'center',
+    minHeight: '35px',
+    minWidth: "280px",
+    marginTop: "35px",
+    background: '#3949ab',
+  },  
   paper: {
     padding: theme.spacing(0),
     textAlign: 'center',
     margin: "50px",
+    minHeight: "200px",
+    minWidth: "400px",
+  },
+  root: {
+    height: "calc(100vh -64px)"
   },
 }));
 
@@ -32,24 +51,43 @@ export default function ElementosRegistro() {
     <div>
       <Grid container spacing={3}>
         <Grid item xs>
-          <h5 className="colortext2 MuiTypography-root makeStyles-title-3 MuiTypography-h6">Registrarse</h5>
-            <Paper>
-              {primerafila()}
-            </Paper>
+              {formulario()}
         </Grid>
       </Grid>        
     </div> 
   );
 }
-
-function primerafila(){
+function formulario() {
   const classes = useStyles();
+  const [values, setValues] = React.useState({
+    name: '',
+    age: '',
+    multiline: 'Controlled',
+  });
+
+  const handleChange = name => event => {
+    setValues({ ...values, [name]: event.target.value });
+  };
 
   return (
-    <div>
-      <Grid container spacing={3}>
-        <Grid item xs>
-              <List component="nav" aria-label="contacts">
+    <div className={classes.root}>
+      <Grid container
+                direction="row"
+                justify="center"
+                alignItems="center">
+        <Paper className={classes.title} square={true}>
+          <h5 className="colortext MuiTypography-root makeStyles-title-3 MuiTypography-h6" >Registrate</h5>
+         </Paper>
+        </Grid>
+        <Paper className={classes.paper} square={true}>
+              <form className={classes.container} noValidate autoComplete="off">
+                <Grid
+                container
+                direction="column"
+                justify="center"
+                alignItems="center"
+                  >
+                <Grid item xs>
                 <ListItem>
                   <TextField
                       id="standard-dense"
@@ -64,6 +102,8 @@ function primerafila(){
                       margin="dense"
                   />
                 </ListItem>
+                </Grid>
+                <Grid item xs>
                 <ListItem>
                   <TextField
                       id="standard-search"
@@ -81,9 +121,36 @@ function primerafila(){
                       margin="normal"
                   />
                 </ListItem>
-            </List>
-        </Grid>
-      </Grid>
+                 </Grid>
+                 <Grid item xs>
+                 <ListItem>
+                  <TextField
+                      id="standard-dense"
+                      label="Telefono celular"
+                      className={clsx(classes.textField, classes.dense)}
+                      margin="normal"
+                  />
+                  <TextField
+                      id="standard-dense"
+                      label="Sexo"
+                      className={clsx(classes.textField, classes.dense)}
+                      margin="dense"
+                  />
+                </ListItem>
+                 </Grid> 
+                </Grid>
+                <Grid
+                    container
+                    direction="row"
+                    justify="center"
+                    alignItems="center"
+                  >
+                  <Button variant="contained" color="primary" className={classes.button}>
+                    Crear cuenta
+                  </Button>
+                </Grid>
+              </form>
+        </Paper>
     </div>
   );
 }
